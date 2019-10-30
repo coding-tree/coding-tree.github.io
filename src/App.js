08feea1e3bb3115e490c50606b30c6100853e78c
freestyle09 <jozefrzadkosz09@gmail.com>
2019-10-30 21:02:47 +0100

    added tree and branches

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './styles/main.scss';
 import Home from './components/Home/Home';
 import Faq from './components/Faq';
 import Footer from './components/Footer';
+import {Switch, Route} from 'react-router-dom';
+import Test from './components/Test/Test';
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-      <Faq></Faq>
+    <React.Fragment>
+      <Switch>
+        <Route exact strict path="/" component={Test}></Route>
+        <Route exact strict path="/home" component={Home}></Route>
+      </Switch>
+      {/* <Home></Home> */}
+      {/* <Faq></Faq> */}
       <Footer></Footer>
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,52 @@
-import React, {useRef, useEffect, useState, useCallback} from 'react';
+import React, {useRef, useEffect, useState} from 'react';
 import treeLeft from './tree_left.svg';
 import treeRight from './tree_right.svg';
-import background from './background.svg';
 import ReactResizeDetector from 'react-resize-detector';
-import {useSpring, useSprings, animated} from 'react-spring';
+import {useSpring, animated} from 'react-spring';
+import Lottie from 'react-lottie';
+import * as animation from './background.json';
+import * as tree from './tree.json';
 
 function Home() {
   const [isLoading, setLoading] = useState(true);
   const [height, setHeight] = useState(null);
-
-  const image = useRef(null);
+  const backgroundImg = useRef(null);
 
   const awaitImage = () => {
     setLoading(false);
   };
 
   const resize = () => {
-    setHeight(image.current.offsetHeight);
+    setHeight(backgroundImg.current.el.offsetHeight);
+  };
+
+  const bodymovinOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation.default,
+    rendererSettings: {
+      // preserveAspectRatio: 'none',
+      className: 'sun',
+    },
   };
 
   useEffect(() => {
     if (!isLoading) {
-      setHeight(image.current.offsetHeight);
+      setHeight(backgroundImg.current.el.offsetHeight);
     }
   });
+
+  const treeOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: tree.default,
+    rendered: 'svg',
+
+    rendererSettings: {
+      progressiveLoad: false,
+      className: 'tree',
+    },
+  };
 
   return (
     <main>
@@ -31,6 +54,7 @@ function Home() {
         <header>
           <div style={{height}} className="logo-container">
             <h1>Coding Tree</h1>
+
             <div className="description">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt explicabo est quas adipisci iste
               provident voluptas facere nostrum culpa a! Numquam, ex officiis. Eveniet dolor, amet dolorem error quia
@@ -40,6 +64,7 @@ function Home() {
               ipsum dignissimos alias et in officia soluta delectus voluptate quidem laboriosam facere dolorum ut, quam
               voluptates fuga eligendi.
             </div>
+
             <TreeButton top="32%" left="30%"></TreeButton>
             <TreeButton top="45.5%" right="31.5%" isLeft={false}></TreeButton>
             <TreeButton top="59%" left="33%"></TreeButton>
@@ -47,19 +72,27 @@ function Home() {
           </div>
         </header>
         <div className="background">
-          <img
-            ref={image}
+          <Lottie
             onLoad={awaitImage}
-            className="background-image"
-            src={background + '#svgView(preserveAspectRatio(none))'}
-            alt=""
-          />
+            ref={backgroundImg}
+            isClickToPauseDisabled={true}
+            speed={1}
+            options={bodymovinOptions}
+          ></Lottie>
         </div>
-        <TreeLeft height={height}></TreeLeft>
+        <Lottie
+          onLoad={awaitImage}
+          // ref={backgroundImg}
+          style={{position: 'absolute', top: '0px', height: 'auto', zIndex: '-1'}}
+          isClickToPauseDisabled={true}
+          speed={1}
+          options={treeOptions}
+        ></Lottie>
+        {/* <TreeLeft height={height}></TreeLeft> */}
 
-        <div style={{height}} className="tree-right">
+        {/* <div style={{height}} className="tree-right">
           <img className="tree-right-image" src={treeRight} alt="" />
-        </div>
+        </div> */}
       </div>
 
       <ReactResizeDetector handleWidth handleHeight onResize={resize} />
@@ -69,22 +102,22 @@ function Home() {
 
 function TreeButton({top, left = 'initial', right = 'initial', isLeft = true}) {
   const [isVisible, setVisibility] = useState(false);
+  const [key, setKey] = useState(0);
   const descAnimation = useSpring({
     width: isVisible ? '420px' : '0px',
     paddingLeft: isVisible ? '20px' : '0px',
     overflow: isLeft ? 'hidden' : '',
     delay: isVisible ? 250 : 0,
-
-    // config: {duration: 300},
   });
   const buttonAnimation = useSpring({
     backgroundColor: !isVisible ? '#4b1220' : '#fff',
+    config: {duration: 1000},
+    // onRest: () => !isVisible && setKey(key + 1),
   });
   const arrowAnimation = useSpring({
     height: isVisible ? '36px' : '0px',
     opacity: isVisible ? 1 : 0,
     delay: isVisible ? 0 : 700,
-
     config: {duration: 300},
     // config: {delay: isVisible ? 2000 : 0},
   });
@@ -93,14 +126,6 @@ function TreeButton({top, left = 'initial', right = 'initial', isLeft = true}) {
     config: {duration: 300},
   });
 
-  const buttonInfAnimation = useSpring({
-    from: {
-      scale: 1,
-    },
-    to: {
-      scale: 1.5,
-    },
-  });
   return (
     <animated.div
       style={{...containerAnimation, top, left, right, transform: !isLeft && 'rotateZ(-180deg)'}}
@@ -108,7 +133,7 @@ function TreeButton({top, left = 'initial', right = 'initial', isLeft = true}) {
     >
       <React.Fragment>
         <animated.button
-          style={{...buttonInfAnimation, ...buttonAnimation}}
+          style={{transform: 'scale(1.3)', ...buttonAnimation}}
           onMouseLeave={() => setVisibility(false)}
           onMouseEnter={() => setVisibility(true)}
           className="hover"
