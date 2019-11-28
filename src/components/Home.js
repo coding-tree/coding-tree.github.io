45ea3c598f127f8fc67413b71ef4239d423bfe36
freestyle09 <jozefrzadkosz09@gmail.com>
2019-11-28 09:31:27 +0100

    links has been updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,13 +11,25 @@ function Home() {
         <div>
           <h2>Znajdź nas na:</h2>
           <div className="social">
-            <a href="">
+            <a
+              href="https://discord.gg/ZevtyMf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-discord"></i>
             </a>
-            <a href="">
+            <a
+              href="https://www.facebook.com/groups/289256475294229/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-facebook-square"></i>
             </a>
-            <a href="">
+            <a
+              href="https://github.com/damian0o"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fab fa-github-square"></i>
             </a>
           </div>
