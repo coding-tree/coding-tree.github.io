a0db6ca5fa540191aa9407bc36d5781b891cbcfb
freestyle09 <jozefrzadkosz09@gmail.com>
2019-11-29 13:22:31 +0100

    menu mobile has been finished

--- src/components/NavigationMobile.js
+++ src/components/NavigationMobile.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import {NavLink} from 'react-router-dom';
+import {useSpring, animated} from 'react-spring';
+import * as easings from 'd3-ease';
+
 const bodyScrollLock = require('body-scroll-lock');
 const disableBodyScroll = bodyScrollLock.disableBodyScroll;
 const enableBodyScroll = bodyScrollLock.enableBodyScroll;
@@ -11,41 +14,50 @@ function NavigationMobile() {
     isVisible ? disableBodyScroll(body) : enableBodyScroll(body);
   });
   const date = new Date();
-  const styles = isVisible
-    ? {
-        width: '100%',
-        height: '100%',
-      }
-    : {};
+
+  const menuAnimation = useSpring({
+    width: isVisible ? '100%' : '0%',
+    height: isVisible ? '100%' : '0%',
+    bottom: isVisible ? '0px' : '35px',
+    right: isVisible ? '0px' : '35px',
+    borderRadius: isVisible ? '0%' : '100%',
+    opacity: isVisible ? 1 : 0,
+    zIndex: isVisible ? 99 : 50,
+    delay: isVisible ? 0 : 400,
+    config: {duration: 500, easing: easings.easeBackInOut},
+  });
+
+  const textAnimation = useSpring({
+    width: isVisible ? '200px' : '0px',
+    opacity: isVisible ? 1 : 0,
+    delay: isVisible ? 400 : 0,
+  });
+
   return (
     <div id="navigation-mobile">
       <h1>
         <img src="/logo.svg" alt="" />
       </h1>
-      <button onClick={() => toggleMenu(true)} className="menu">
-        <i className="fas fa-ellipsis-v"></i>
+      <button onClick={() => toggleMenu(prev => !prev)} className="menu">
+        <i className={isVisible ? 'fas fa-times' : 'fas fa-ellipsis-v'}></i>
       </button>
 
-      <nav style={styles}>
-        <h1>
-          <img src="/logo.svg" alt="" />
-        </h1>
-        <button className="close" onClick={() => toggleMenu(false)}>
-          <i className="fas fa-times"></i>
-        </button>
-        <div>
+      <animated.nav style={menuAnimation}>
+        <animated.h1 style={textAnimation}>
+          <animated.img style={textAnimation} src="/logo.svg" alt="" />
+        </animated.h1>
+        <animated.div style={textAnimation}>
           <NavLink exact strict to="/">
             Strona główna
           </NavLink>
-
           <NavLink exact strict to="/o-nas">
             Członkowie
           </NavLink>
-        </div>
-        <footer>
+        </animated.div>
+        <animated.footer style={textAnimation}>
           <p>Copyright &copy; {date.getFullYear()} | Coding Tree </p>
-        </footer>
-      </nav>
+        </animated.footer>
+      </animated.nav>
     </div>
   );
 }
