a6ca3ef03d6768166c0fa4355e49cafa4251f2e6
Józef Rzadkosz <jozefrzadkosz@Jozefs-iMac.local>
2020-02-26 09:13:08 +0100

    added basic styles

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ const NotFound = lazy(() => import('./components/NotFound'));
 
 function App() {
   const calculateViewportHeight = () => {
-    let vh = window.innerHeight * 0.01;
+    const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   };
   useEffect(() => {
-    let vh = window.innerHeight * 0.01;
+    const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     window.addEventListener('resize', calculateViewportHeight);
     return () => {
--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,41 +1,21 @@
-import React, {useEffect, useState, lazy} from 'react';
+import React from 'react';
 
-const Navigation = lazy(() => import('./Navigation'));
-const NavigationMobile = lazy(() => import('./NavigationMobile'));
-const Team = lazy(() => import('./Team'));
-const TeamPerson = lazy(() => import('./TeamPerson'));
-
-function AboutUs() {
-  const [documentWidth, setDocumentWidth] = useState(null);
-  const checkDeviceWidth = () => {
-    setDocumentWidth(document.body.offsetWidth);
-  };
-  useEffect(() => {
-    setDocumentWidth(document.body.offsetWidth);
-    window.addEventListener('resize', checkDeviceWidth);
-    return () => {
-      window.removeEventListener('resize', checkDeviceWidth);
-    };
-  }, [documentWidth]);
-
-  const isDesktop = documentWidth > 896;
-
+const AboutUs = () => {
   return (
     <section id="about-us">
-      <div className="container">
-        {isDesktop ? <Navigation></Navigation> : <NavigationMobile></NavigationMobile>}
-        <Team
-          title="Nasz Team"
-          subtitle="Lorem ipsum Sit"
-          description="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa dolorum obcaecati vel commodi, molestiae pariatur quasi repudiandae odio deserunt">
-          <TeamPerson src="/images/1.jpg" name="Damian Ospara"></TeamPerson>
-          <TeamPerson src="/images/2.jpg" name="Józef Rzadkosz"></TeamPerson>
-          <TeamPerson src="/images/3.jpg" name="Jakub Wojtoń"></TeamPerson>
-          <TeamPerson src="/images/4.jpg" name="Łukasz Strobejko"></TeamPerson>
-        </Team>
+      <div className="background">
+        <img src="" alt="" />
+      </div>
+
+      {/* Container */}
+      <div className="box">
+        <img src="box.svg" alt="" />
+        <div className="box-content">
+          <h1>Works</h1>
+        </div>
       </div>
     </section>
   );
-}
+};
 
 export default AboutUs;
