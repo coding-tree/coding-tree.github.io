63c8f86c7f5a9eb3f5287068cf97bb278351d0b9
freestyle09 <jozefrzadkosz09@gmail.com>
2020-03-02 19:30:54 +0100

    fixed styles and added avatar

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,6 +1,49 @@
 import React, {useState} from 'react';
 import Navigation from './Navigation';
 
+const fakeData = [
+  {
+    name: 'Coding Tree',
+    kind: 'folder',
+    children: [
+      {id: 1, kind: 'folder', name: 'Czarny Pas', children: [{id: 1, kind: 'person', name: 'Damian Ospara'}]},
+      {
+        id: 2,
+        kind: 'folder',
+        name: 'Brązowy Pas',
+        children: [
+          {id: 1, kind: 'person', name: 'Józef Rzadkosz'},
+          {id: 2, kind: 'person', name: 'Jakub Wojtoń'},
+        ],
+      },
+      {
+        id: 3,
+        kind: 'folder',
+        name: 'Zielony Pas',
+        children: [
+          {
+            id: 1,
+            kind: 'person',
+            name: 'Józef Rzadkosz',
+          },
+          {id: 2, kind: 'person', name: 'Jakub Wojtoń'},
+        ],
+      },
+      {
+        id: 4,
+        kind: 'folder',
+        name: 'Żółty Pas',
+        children: [
+          {id: 1, kind: 'person', name: 'Kazimierz Bargowski'},
+          {id: 2, kind: 'person', name: 'Emil Sankowski'},
+          {id: 3, kind: 'person', name: 'Paweł Wojtkiewicz'},
+          {id: 4, kind: 'person', name: 'Jędrzej Paulus'},
+        ],
+      },
+    ],
+  },
+];
+
 const AboutUs = () => {
   const [fakePath, setFakePath] = useState(`C:\\Coding Tree\\Czarny Pas\\Damian Ospara `);
   return (
@@ -49,63 +92,13 @@ const AboutUs = () => {
 
 export default AboutUs;
 
-const fakeData = {
-  name: 'Coding Tree',
-  kind: 'folder',
-  children: [
-    {id: 1, kind: 'folder', name: 'Czarny Pas', children: [{id: 1, name: 'Damian Ospara'}]},
-    {
-      id: 2,
-      kind: 'folder',
-      name: 'Brązowy Pas',
-      children: [
-        {id: 1, kind: 'person', name: 'Józef Rzadkosz'},
-        {id: 2, kind: 'person', name: 'Jakub Wojtoń'},
-      ],
-    },
-    {
-      id: 3,
-      kind: 'folder',
-      name: 'Zielony Pas',
-      children: [
-        {
-          id: 1,
-          kind: 'folder',
-          name: 'Józef Rzadkosz',
-        },
-        {id: 2, kind: 'person', name: 'Jakub Wojtoń'},
-      ],
-    },
-    {
-      id: 4,
-      kind: 'folder',
-      name: 'Żółty Pas',
-      children: [
-        {id: 1, kind: 'person', name: 'Kazimierz Bargowski'},
-        {id: 2, kind: 'person', name: 'Emil Sankowski'},
-        {id: 3, kind: 'person', name: 'Paweł Wojtkiewicz'},
-        {id: 4, kind: 'person', name: 'Jędrzej Paulus'},
-      ],
-    },
-  ],
-};
-
 const Folders = () => {
-  const [isExpanded, setExpand] = useState(false);
-
   const setFolderIcon = isVisible => {
     return isVisible ? '-' : '+';
   };
   return (
     <div>
-      <div className="parent-folder">
-        <div onClick={() => setExpand(!isExpanded)} className="expand">
-          {setFolderIcon(isExpanded)}
-        </div>
-        <i className="fas fa-folder"></i>
-        {fakeData.name}
-      </div>
-      {fakeData.children.map(el => (
+      {fakeData.map(el => (
         <SubFolder key={el.id} setFolderIcon={setFolderIcon} el={el}></SubFolder>
       ))}
     </div>
@@ -114,19 +107,18 @@ const Folders = () => {
 
 const SubFolder = ({el, setFolderIcon}) => {
   const [isExpanded, setExpand] = useState(false);
+  const [isSelected, setSelection] = useState(false);
   const {id, name, kind, children} = el;
-  const changeIcon = kind => {
-    return kind === 'folder' ? 'fas fa-folder' : '';
-  };
+
   return (
-    <div key={id} className="folder">
-      <div className="folder-kind">
+    <div key={id} onClick={() => setSelection(!isSelected)} className={isSelected ? 'folder selected' : 'folder'}>
+      <div onClick={() => setExpand(!isExpanded)} className="folder-kind">
         {kind === 'folder' && (
           <div onClick={() => setExpand(!isExpanded)} className="expand">
             {setFolderIcon(isExpanded)}
           </div>
         )}
-        <i className={changeIcon(kind)}></i>
+        {kind === 'folder' ? <i className="fas fa-folder"></i> : <img src="pawel_woj.svg" />}
         <p> {name} </p>
       </div>
       {children !== undefined &&
