834210ee05c85cbf2cbead8fb301361166a5e0e0
freestyle09 <jozefrzadkosz09@gmail.com>
2020-03-06 17:21:16 +0100

    finished profile detail && added circle avatars

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,60 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Navigation from './Navigation';
-
-const fakeData = [
-  {
-    id: 1,
-    name: 'Coding Tree',
-    kind: 'folder',
-    children: [
-      {id: 1, kind: 'folder', name: 'Czarny Pas', children: [{id: 1, kind: 'person', name: 'Damian Ospara'}]},
-      {
-        id: 2,
-        kind: 'folder',
-        name: 'Brązowy Pas',
-        children: [
-          {id: 1, kind: 'person', name: 'Józef Rzadkosz'},
-          {id: 2, kind: 'person', name: 'Jakub Wojtoń'},
-        ],
-      },
-      {
-        id: 3,
-        kind: 'folder',
-        name: 'Zielony Pas',
-      },
-      {
-        id: 4,
-        kind: 'folder',
-        name: 'Żółty Pas',
-        children: [
-          {id: 1, kind: 'person', name: 'Kazimierz Bargowski'},
-          {id: 2, kind: 'person', name: 'Emil Sankowski'},
-          {id: 3, kind: 'person', name: 'Łukasz Strobejko'},
-          {id: 4, kind: 'person', name: 'Paweł Wojtkiewicz'},
-          {id: 5, kind: 'person', name: 'Jędrzej Paulus'},
-        ],
-      },
-      {
-        id: 5,
-        kind: 'folder',
-        name: 'Niebieski Pas',
-        children: [],
-      },
-      {
-        id: 6,
-        kind: 'folder',
-        name: 'Biały Pas',
-        children: [
-          {id: 1, kind: 'person', name: 'Arkadiusz Świokło'},
-          {id: 2, kind: 'person', name: 'Bartosz Kamiński'},
-          {id: 3, kind: 'person', name: 'Paweł Kochowicz'},
-          {id: 4, kind: 'person', name: 'Paweł Marciniak'},
-          {id: 5, kind: 'person', name: 'Piotr Rasztemborski'},
-        ],
-      },
-    ],
-  },
-];
+import folderStructure from '../data/folderStructure.json';
 
 const AboutUs = () => {
   const [fakePath, setFakePath] = useState(`C:\\Coding Tree\\Czarny Pas\\Damian Ospara `);
@@ -64,11 +10,13 @@ const AboutUs = () => {
     <section id="about-us">
       <Navigation></Navigation>
 
+      {/* // * BACKGROUND IMAGE */}
       <div className="background">{/* <BackgroundLarge></BackgroundLarge> */}</div>
 
-      {/* Container */}
+      {/* // * BOX */}
       <div className="box">
         <img src="box.svg" alt="" />
+
         <div className="box-wrapper">
           <div className="box-path">
             <div>
@@ -90,16 +38,25 @@ const AboutUs = () => {
           <div className="box-content">
             {/*  LEFT BOX */}
             <div className="box-folders">
-              <Folders setSelectedElement={setSelectedElement}></Folders>
+              <Folders folderStructure={folderStructure} setSelectedElement={setSelectedElement}></Folders>
             </div>
 
             {/* RIGHT BOX */}
             <div className="box-info">
-              {selectedElement && (
-                <div>
-                  <h3>
-                    {selectedElement.kind}: {selectedElement.name}
-                  </h3>
+              {selectedElement ? (
+                selectedElement.kind === 'person' ? (
+                  <ProfileDetail profil={selectedElement}></ProfileDetail>
+                ) : (
+                  <FolderDetail items={selectedElement.children} name={selectedElement.name}></FolderDetail>
+                )
+              ) : (
+                <div className="box-info-startup">
+                  <h2>Witamy na widoku folderów</h2>
+                  <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis officiis vitae praesentium
+                    ducimus iste incidunt voluptates cum autem quis, totam beatae. Animi fuga expedita atque labore
+                    reiciendis magni harum obcaecati.
+                  </p>
                 </div>
               )}
             </div>
@@ -112,7 +69,99 @@ const AboutUs = () => {
 
 export default AboutUs;
 
-const Folders = ({setSelectedElement}) => {
+const ProfileDetail = ({profil}) => {
+  const {
+    name,
+    landscape,
+    github,
+    fb,
+    dateOfBirth,
+    stack,
+    stackOverflow,
+    twitter,
+    linkedIn,
+    portfolio,
+    description,
+  } = profil;
+
+  const date = new Date().getFullYear();
+
+  const calculateAge = dateOfBirth => {
+    return date - dateOfBirth;
+  };
+
+  return (
+    <div>
+      <div className="avatar">
+        <img src={`avatars/${landscape}.svg`} alt="" />
+      </div>
+      <div className="profile">
+        <div className="profile-name">
+          <h2>{name}</h2>
+          <div className="profile-social">
+            {github && (
+              <a href={github} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-github"></i>
+              </a>
+            )}
+            {fb && (
+              <a href={fb} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-facebook"></i>
+              </a>
+            )}
+            {stackOverflow && (
+              <a href={stackOverflow} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-stack-overflow"></i>
+              </a>
+            )}
+            {twitter && (
+              <a href={twitter} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-twitter-square"></i>
+              </a>
+            )}
+            {linkedIn && (
+              <a href={linkedIn} target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-linkedin"></i>
+              </a>
+            )}
+            {portfolio && (
+              <a href={portfolio} target="_blank" rel="noopener noreferrer">
+                <i className="fas fa-globe"></i>
+              </a>
+            )}
+          </div>
+        </div>
+        {dateOfBirth && <div className="profile-age">Wiek: {calculateAge(dateOfBirth)} lat</div>}
+
+        {stack && (
+          <div className="profile-stack">
+            <p>
+              Technologie:{' '}
+              {stack.length > 1 ? stack.map(el => <span key={el.id}>{el.title}, </span>) : <span>{stack}</span>}
+            </p>
+          </div>
+        )}
+
+        {description ? (
+          <div className="profile-description">{description}</div>
+        ) : (
+          <div className="profile-description">Brak opisu profilu</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const FolderDetail = ({name, items = []}) => {
+  return (
+    <div className="folder-detail">
+      <h2>{name}</h2>
+      <p>Ilość plików w folderze: {items.length}</p>
+    </div>
+  );
+};
+
+const Folders = ({setSelectedElement, folderStructure}) => {
   const [selectedItem, setSelection] = useState(null);
   const setFolderIcon = isVisible => {
     return isVisible ? '-' : '+';
@@ -122,10 +171,10 @@ const Folders = ({setSelectedElement}) => {
   }, [selectedItem]);
   return (
     <div>
-      {fakeData.map(el => (
+      {folderStructure.map(el => (
         <SubFolder
           key={el.id}
-          fakeData={fakeData}
+          folderStructure={folderStructure}
           selectedItem={selectedItem}
           setSelection={setSelection}
           setFolderIcon={setFolderIcon}
@@ -136,12 +185,12 @@ const Folders = ({setSelectedElement}) => {
   );
 };
 
-const SubFolder = ({el, setFolderIcon, fakeData, selectedItem, setSelection, setSelectedElement}) => {
+const SubFolder = ({el, setFolderIcon, folderStructure, selectedItem, setSelection, setSelectedElement}) => {
   const [isExpanded, setExpand] = useState(false);
-  const {id, name, kind, children} = el;
+  const {id, name, kind, children, avatar} = el;
 
   const styles = {
-    height: fakeData.length === id && kind === 'person' && '30px',
+    height: folderStructure.length === id && kind === 'person' && '30px',
     top: kind === 'person' && '-15px',
   };
 
@@ -155,7 +204,7 @@ const SubFolder = ({el, setFolderIcon, fakeData, selectedItem, setSelection, set
 
   return (
     <div key={id} className="folder">
-      {(fakeData.length !== id || kind === 'person') && <hr className="border-top" style={styles} />}
+      {(folderStructure.length !== id || kind === 'person') && <hr className="border-top" style={styles} />}
 
       <div onClick={e => handleFolderClick(e)} className="folder-kind">
         {/* BORDER */}
@@ -166,7 +215,7 @@ const SubFolder = ({el, setFolderIcon, fakeData, selectedItem, setSelection, set
             {setFolderIcon(isExpanded)}
           </div>
         )}
-        {kind === 'folder' ? <i className="fas fa-folder"></i> : <img src="pawel_woj.svg" />}
+        {kind === 'folder' ? <i className="fas fa-folder"></i> : <img src={`avatars/${avatar}.svg`} />}
         <p> {name} </p>
       </div>
 
@@ -176,7 +225,7 @@ const SubFolder = ({el, setFolderIcon, fakeData, selectedItem, setSelection, set
         children.map(el => (
           <SubFolder
             key={el.id}
-            fakeData={children}
+            folderStructure={children}
             selectedItem={selectedItem}
             setSelection={setSelection}
             setFolderIcon={setFolderIcon}
